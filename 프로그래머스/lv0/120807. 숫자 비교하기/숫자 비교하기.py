7c7712319e52af65f2c9def220a7f88526d034ba
dongju923 <81959658+dongju923@users.noreply.github.com>
2023-03-06 17:51:09 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -0,0 +1,9 @@
+def solution(num1, num2):
+    if num1 == num2:
+        return 1
+    else:
+        return -1
+    # 한줄표현이 가능
+    # num1 == num2가 같으면 1을 반환, 아니면 -1을 반환
+    # retrun 1 if num1 == num2 else -1
+    
