7c9f3cdf1f34bb65051caa2f30c03c6ab0c17939
dongju923 <81959658+dongju923@users.noreply.github.com>
2023-03-16 19:00:58 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,6 +1,18 @@
+
 def solution(array, height):
     count = 0
     for i in array:
         if i > height:
             count += 1
     return count
+
+# 다른풀이
+"""
+def solution(array, height):
+    # array에 height원소 추가
+    array.append(height)
+    # height보다 키가 큰사람을 구하는 것이 목표이므로 내림차순 정렬
+    array.sort(reverse=True)
+    # array내에서 height가 있는 인덱스 반환
+    return array.index(height)
+"""
