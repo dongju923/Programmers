1cb7d2b82985030cb8960d354bb82df6d360afce
dongju923 <81959658+dongju923@users.noreply.github.com>
2023-03-17 15:27:08 +0900

    Update 배열 회전시키기.py

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,6 +1,8 @@
 def solution(numbers, direction):
     if direction == 'right':
+        # 맨 뒤값을 맨 앞으로 보내고, 나머지를 그대로
         return [numbers[-1]] + numbers[0:-1]
     else:
+        # 맨 앞값을 뒤로 보내고, 나머지를 그대로
         return numbers[1:] + [numbers[0]]
     #return [numbers[-1]] + numbers[0:-1] if direction=='right' else numbers[1:] + [numbers[0]]
