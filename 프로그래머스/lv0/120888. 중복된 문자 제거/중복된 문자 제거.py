bc900a01c9100e50e7c7f9986e6dae419036efb0
dongju923 <81959658+dongju923@users.noreply.github.com>
2023-03-18 14:43:23 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -0,0 +1,9 @@
+# dict.fromkeys() -> 문자열을 딕셔너리 키로 만듦. 딕셔너리 키는 중복이 안됨
+def solution(my_string):
+    # 문자열을 딕셔너리의 키로 변환. 중복은 없어짐
+    dic = dict.fromkeys(my_string)
+    # dic의 키를 리스트에 담음
+    li = list(dic.keys())
+    # 리스트내 문자열을 join으로 문자열로 만듦
+    return ''.join(li)
+    #return ''.join(dict.fromkeys(my_string))
